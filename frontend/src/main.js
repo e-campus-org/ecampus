382760c6135931d56f62a6d095d778bd88027536
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2024-07-13 10:52:52 +0300

    fix: removed unused components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,20 +1,23 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
+import App from "./App.vue";
+import { createApp } from "vue";
+import { createVuetify } from "vuetify";
+import { createPinia } from "pinia";
+import router from "@/router";
+import { md3 } from "vuetify/blueprints";
 
-// Plugins
-import { registerPlugins } from '@/plugins'
+import "@mdi/font/css/materialdesignicons.css";
 
-// Components
-import App from './App.vue'
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: "dark",
+  },
+  blueprint: md3,
+});
 
-// Composables
-import { createApp } from 'vue'
+const pinia = createPinia();
 
-const app = createApp(App)
+const app = createApp(App);
 
-registerPlugins(app)
+app.use(vuetify).use(router).use(pinia);
 
-app.mount('#app')
+app.mount("#app");
